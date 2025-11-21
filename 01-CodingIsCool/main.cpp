a8dc03784d66521ba42a78c454d633d83adff8ed
CristianD299 <2409-123177@esl.ac.uk>
2025-11-21 09:44:56 +0000

    Update main.cpp

--- 01-CodingIsCool/main.cpp
+++ 01-CodingIsCool/main.cpp
@@ -3,7 +3,7 @@ using namespace std;
 
 // The program prints a simple message to the console.
 int main() {
-    // Output the message
+    // Output a given message
     cout << "Coding is Cool!" << endl;
     return 0;
 }
