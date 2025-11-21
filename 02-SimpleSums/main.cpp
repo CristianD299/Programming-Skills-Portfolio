98197aa5884a5d8d7261c6b7ce26e23c85240103
CristianD299 <2409-123177@esl.ac.uk>
2025-11-21 09:21:55 +0000

    Update main.cpp

--- 02-SimpleSums/main.cpp
+++ 02-SimpleSums/main.cpp
@@ -5,7 +5,7 @@ using namespace std;
 int main() {
     int a, b;
     cout << "Enter two numbers: ";
-    cin >> a >> b; // Read two integers from user
+    cin >> a >> b;
     // Shows the sum of the two numbers
     cout << "Sum: " << a + b << endl;
     return 0;
