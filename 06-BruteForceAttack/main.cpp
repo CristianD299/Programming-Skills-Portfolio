570c98686111118e1b7e4a8fc5da4889fe75dc00
CristianD299 <2409-123177@esl.ac.uk>
2025-11-21 09:42:22 +0000

    Update main.cpp

--- 06-BruteForceAttack/main.cpp
+++ 06-BruteForceAttack/main.cpp
@@ -4,19 +4,18 @@ using namespace std;
 
 // This program simulates a brute force attack by repeatedly asking for a password until correct.
 int main() {
-    string password = "1234"; // The correct password
+    string password = "password"; // The correct password
     string guess; // Variable for user's guess
     int attempts = 0; // Counter for attempts
 
-    cout << "Brute Force Attack Simulation
-";
+    cout << "Brute Force Attack Simulation" << endl;
     do {
-        cout << "Enter password guess: ";
+        cout << "Enter password: ";
         cin >> guess;
         attempts++; // Increment attempts
     } while (guess != password);
 
-    // Display number of attempts taken
+    // Display number of attempts 
     cout << "Password cracked in " << attempts << " attempts!" << endl;
     return 0;
 }
